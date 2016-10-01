c1b9a10272595d43148d29ef997ecd16c67e89ad
hsykwon <hsykwon@gmail.com>
2016-10-01 01:17:32 -0700

    Added a resultsPage.html and updated the main.py

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import jinja2
 import os
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_environment = jinja2.Environment( loader=jinja2.FileSystemLoader('templates'))
 
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('mainPage.html')
-        self.response.out.write(template.render())
+        main_template = env.get_template('mainPage.html')
+        self.response.out.write(main_template.render())
+    def post(self):
+        complete_template = env.get_template('resultsPage.html')
+        template_variables = {
+            'budget_name':self.request.get("budget_name"),
+            'budget_spent':self.request.get("budget_spent"),
+            }
+        self.response.out.write(complete_template.render(template_variables))
 
-routes = [
-    ('/', MainPageHandler),
-]
+app = webapp2.WSGIApplication([
+ ('/', MainPageHandler),
+], debug=True)
