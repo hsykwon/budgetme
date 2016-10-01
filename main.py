6686c435924cf052dd6c9ed13d1b4d7d0bb0dcca
hsykwon <hsykwon@gmail.com>
2016-10-01 09:45:31 -0700

    fixed main.py and mainPage.html

--- main.py
+++ main.py
@@ -11,14 +11,36 @@ class MainPageHandler(webapp2.RequestHandler):
         self.response.out.write(main_template.render())
     def post(self):
         complete_template = env.get_template('mainPage.html')
+        # template_variables = {
+        #     'budget_name':self.request.get("budget_name"),
+        #     'budget_spent':self.request.get("budget_spent"),
+        #     'income':self.request.get("income"),
+        #     'budget':self.request.get("budget"),
+        #     'spendings':self.request.get("spendings"),
+        #     }
+        # self.response.out.write(complete_template.render(template_variables))
+        income_main = self.request.get("income")
+        if income_main != "":
+            income_total = income_total + income_main
+            income_temp = income_total
+            income_main = income_temp
+        budget_main = self.request.get("budget")
+        if budget_main != "":
+            budget_total = budget_total + budget_main
+            budget_temp = budget_total
+            budget_main = budget_temp
+        spendings_main = self.request.get("spendings")
+        if spendings_main != "":
+            spendings_total = spendings_total + spendings_main
+            spendings_temp = spendings_total
+            spendings_main = spendings_temp
         template_variables = {
-            'budget_name':self.request.get("budget_name"),
-            'budget_spent':self.request.get("budget_spent"),
             'income':self.request.get("income"),
             'budget':self.request.get("budget"),
             'spendings':self.request.get("spendings"),
-            }
+        }
         self.response.out.write(complete_template.render(template_variables))
+
 
 class LoginHandler(webapp2.RequestHandler):
     # def get(self):
