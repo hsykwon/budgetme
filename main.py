1aa0dd207166e5e70b25f36a415dcd2cf6493d56
Amy Yee <amyyee@Amys-MacBook-Pro.local>
2016-10-01 02:26:39 -0700

    linked login handler, but still blank

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ class MainPageHandler(webapp2.RequestHandler):
     def post(self):
         complete_template = env.get_template('resultsPage.html')
         template_variables = {
-            'income':self.request.get('income'),
-            'budget':self.request.get("budget"),
+            'budget_name':self.request.get("budget_name"),
+            'budget_spent':self.request.get("budget_spent"),
             }
         self.response.out.write(complete_template.render(template_variables))
 
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        login_template = env.get_template('login.html')
+        self.response.out.write(login_template.render())
+
 app = webapp2.WSGIApplication([
  ('/', MainPageHandler),
+ ('/login', LoginHandler)
 ], debug=True)
